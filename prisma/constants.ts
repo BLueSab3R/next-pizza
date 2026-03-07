e68e15fe3a969b1da06f1ba29cfdb347466fa1fc
BLueSab3R <setunov9@gmail.com>
2026-03-07 20:19:28 +0100

    add new data for db

---
+++ prisma/constants.ts
@@ -0,0 +1,132 @@
+export const categories = [
+  { name: "Pizzas" },
+  { name: "Combos" },
+  { name: "Snacks" },
+  { name: "Cocktails" },
+  { name: "Coffee" },
+  { name: "Drinks" },
+  { name: "Desserts" },
+];
+
+export const ingredients = [
+  {
+    name: "Pepperoni Lovers",
+    price: 109,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/4/FA35B15078575E1B43939C02B7EAC9E0D/760x706.jpg",
+  },
+  {
+    name: "Pulled Beef BBQ",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/68/82CF8DA4494063613FC66326F067A35CD/760x706.jpg",
+  },
+  {
+    name: "Vesuvio",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/74/B3B1A320DAB652FDBCE346B348D1C042D/760x706.jpg",
+  },
+  {
+    name: "Kebab Lovers",
+    price: 109,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/64/CA4905E2C540DE2F5425DBDC0C85F6B2D/760x706.jpg",
+  },
+  {
+    name: "Bacon BBQ",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/49/AB88BEFAE90C3AC856B9E4E4639695C3D/760x706.jpg",
+  },
+  {
+    name: "Capricciosa",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/50/1BE86216256A7B00CD2F2FE33FABFEC3D/760x706.jpg",
+  },
+  {
+    name: "Hawaii",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/51/06C4BCAE82160D4BD47E119D83B5CE76D/760x706.jpg",
+  },
+  {
+    name: "Spicy Hot",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/66/3D1F3C63638740958A3E59275C056424D/760x706.jpg",
+  },
+  {
+    name: "Supreme",
+    price: 129,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/67/DFE86A18F45E5EC9FB44E31B70F42E8CD/760x706.jpg",
+  },
+  {
+    name: "Al Tonno",
+    price: 109,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/2728/B14EF84B051E716B7BC775BC7B800353D/760x706.jpg",
+  },
+  {
+    name: "Spicy Tuna",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/2735/010CB95433472B6E286C351D5D060C2ED/760x706.jpg",
+  },
+  {
+    name: "Kycklingcurry",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/2759/9A5B433C6590C366E8830130B246AAF4D/760x706.jpg",
+  },
+  {
+    name: "Roasted Garlic",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/73/F5170E07210B4715AB031152D45569F8D/760x706.jpg",
+  },
+  {
+    name: "Chicken Supreme",
+    price: 129,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/63/02F8CA2D0F18FA88748D76F92B429927D/760x706.jpg",
+  },
+  {
+    name: "Chicken Hawaii",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/1371/D6F23F89A9C7E50710A42D9718C6ECA4D/760x706.jpg",
+  },
+  {
+    name: "Margherita",
+    price: 109,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/53/714726F64927884FF856D53EBE936E59D/760x706.jpg",
+  },
+  {
+    name: "Vegeroni Lovers",
+    price: 109,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/2762/5703BD6B491ACFE9F631604CB8055BCAD/760x706.jpg",
+  },
+  {
+    name: "Spicy Vegetarian",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/57/EB7B86FE58B755112F3CD02ABAAB1289D/760x706.jpg",
+  },
+  {
+    name: "Fresh Basil",
+    price: 119,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/54/3B559784137108562F759A50302C4573D/760x706.jpg",
+  },
+  {
+    name: "Veggie Lovers",
+    price: 109,
+    imageUrl:
+      "https://pizzahut-images.futureordering.com/images/product/58/D3A19FDD5ECDC76FC68A633F18E13260D/760x706.jpg",
+  },
+];
