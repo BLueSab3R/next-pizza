defc8d060d79b56cc15fe3d43aaf69005dee17c1
BLueSab3R <setunov9@gmail.com>
2026-03-25 19:32:34 +0100

    add custom hook

---
+++ components/shared/hooks/useClickAway.ts
@@ -0,0 +1,20 @@
+import { RefObject, useEffect } from "react";
+
+export const useClickAway = <T extends HTMLElement>(
+  ref: RefObject<T>,
+  handler: () => void,
+) => {
+  useEffect(() => {
+    const element = ref.current;
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!element || element.contains(event?.target as Node)) return;
+      handler();
+    };
+    document.addEventListener("touchstart", listener);
+    document.addEventListener("mousedown", listener);
+    return () => {
+      document.removeEventListener("touchstart", listener);
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [ref, handler]);
+};
