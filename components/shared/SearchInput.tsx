280d6dfd1e066b6c82e9ce59aac72b73643360b0
BLueSab3R <setunov9@gmail.com>
2026-03-26 20:50:30 +0100

    add photo url to next.config Use axios Create api services

--- components/shared/SearchInput.tsx
+++ components/shared/SearchInput.tsx
@@ -2,33 +2,66 @@
 
 import { Search } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
+import { useClickAway } from "./hooks/useClickAway";
+import Link from "next/link";
+import Image from "next/image";
+
 interface Props {
   className?: string;
 }
 
 export const SearchInput = ({ className }: Props) => {
+  const searchRef = useRef<HTMLDivElement>(null);
   const [focused, setFocused] = useState(false);
 
-  useEffect(() => {}, []);
+  useClickAway(searchRef, () => {
+    setFocused(false);
+  });
+  const onClickItem = () => {
+    console.log("item clicked");
+  };
   return (
     <>
       {focused && (
         <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30" />
       )}
       <div
+        ref={searchRef}
         className={cn(
           "flex rounded-2xl flex-1 justify-between relative h-11 z-30",
           className,
         )}
       >
         <Search className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-500" />
+
         <input
           placeholder="Search pizza..."
           type="text"
           className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
           onFocus={() => setFocused(true)}
         />
+        <div
+          className={cn(
+            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all  duration-300 invisible opacity-0    z-30",
+            focused && "visible opacity-100 top-12",
+          )}
+        >
+          <Link
+            onClick={onClickItem}
+            href="/product/1"
+            className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10 cursor-pointer"
+          >
+            <Image
+              className="rounded-sm "
+              src="https://pizzahut-images.futureordering.com/images/product/4/FA35B15078575E1B43939C02B7EAC9E0D/760x706.jpg"
+              width={32}
+              height={32}
+              alt="pizza"
+            />
+            <span>pizza 1</span>
+          </Link>
+        </div>
       </div>
     </>
   );
