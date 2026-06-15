5b1580d545b7b7f02237b2085856ab6eba1dc09f
BLueSab3R <setunov9@gmail.com>
2026-06-15 18:51:37 +0200

    fix icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link data-rh="true" rel="icon" href="/logo.png" />
-      </head>
       <body className={`${nunito.variable} font-sans antialiased`}>
         {children}
       </body>
